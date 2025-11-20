391e97663063b720c88c2cb4b0887b45c20e410a
Lucas Béfay <lucas.befay@cesjb.be>
2025-11-20 15:55:05 +0100

    Créer plusieurs étudiants

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 import Etudiant from "./Etudiant.js";
 
+var Etudiant1 = new Etudiant("Riri",32,"informatique")
+var Etudiant2 = new Etudiant("Roro",30,"informatique")
+var Etudiant3 = new Etudiant("Rara",33,"informatique")
+
